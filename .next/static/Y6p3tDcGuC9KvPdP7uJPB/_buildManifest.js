2ede6f31ff057bd96cd4cd92851200fb0d0d7bec
Apple <apple@iMac-de-Apple.local>
2026-04-27 22:41:40 -0700

    remove next cache

--- .next/static/Y6p3tDcGuC9KvPdP7uJPB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/688-2bfe041fd9e7d03b.js","static/chunks/pages/index-8816448e6877c37a.js"],"/_error":["static/chunks/pages/_error-56a1a2eec59e6cbd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
